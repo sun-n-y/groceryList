8363f2b317b76bfcbb0157ef95b6dfb0609d00f7
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-10 17:27:00 -0400

    add gorocery list js add items feature

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ const form = document.querySelector('.form-container');
 const alert = document.querySelector('.alert');
 const input = document.querySelector('#input');
 const submitBtn = document.querySelector('.submit-btn');
+const listContainer = document.querySelector('.list-container');
 
 let editFlag = false;
 let editID = '';
-let editBtn = 'edit';
+let submitBtn1 = 'submit';
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (!editFlag && input.value == '') {
+  const inputValue = input.value;
+  if (!editFlag && !inputValue) {
     showAlert('please enter value', 'fail');
+  } else if (!editFlag && inputValue) {
+    const id = new Date().getTime();
+    const element = document.createElement('div');
+    element.classList.add('list-item');
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="item-info">${inputValue}</p>
+                <div class="btn-container">
+                    <button class="edit-btn">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </button>
+                    <button class="trash-btn">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>`;
+    listContainer.appendChild(element);
+    showAlert('item added', 'sucess');
+    restoreForm();
   }
 });
 
@@ -21,4 +41,10 @@ function showAlert(text, alertType) {
     alert.textContent = ``;
     alert.classList.remove(alertType);
   }, 2000);
+}
+
+function restoreForm() {
+  input.value = '';
+  editFlag = false;
+  submitBtn1 = 'submit';
 }
